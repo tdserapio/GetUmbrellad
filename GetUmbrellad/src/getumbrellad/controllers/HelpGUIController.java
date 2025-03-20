9098db1a13b3a52925dcb7502a603be057f8604e
SieggyCajucom <sieggycajucom@gmail.com>
2025-03-21 02:05:56 +0800

    Create HelpGUIController.java

---
+++ GetUmbrellad/src/getumbrellad/controllers/HelpGUIController.java
@@ -0,0 +1,49 @@
+
+
+
+public class HelpGUIController implements ActionListener, MouseListener {
+    
+    private HelpGUI helpGUI;
+    private JButton menuButton;
+    
+    public HelpGUIController(HelpGUI helpGUI, JButton menuButton) {
+        
+        this.helpGUI = helpGUI;
+        this.menuButton = menuButton;
+        
+    }
+    
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == menuButton) {
+            var display = new mainMenu();
+            display.setVisible(true);
+            helpGUI.dispose();
+        }
+    }
+    
+    
+    @Override
+    public void mouseClicked(MouseEvent e) {}
+    @Override
+    public void mousePressed(MouseEvent e) {}
+    @Override
+    public void mouseReleased(MouseEvent e) {}
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        if (e.getSource() == menuButton) {
+            menuButton.setBackground(Color.gray);
+        }
+        
+    }
+    @Override
+    public void mouseExited(MouseEvent e) {
+        if (e.getSource() == menuButton) {
+            menuButton.setBackground(null);
+        }
+        
+    }
+
+    
+}
