33a5de9a846f25e2d3ccb409bae17c58df77ae07
SieggyCajucom <sieggycajucom@gmail.com>
2025-04-02 07:52:40 +0800

    Add files via upload

---
+++ GetUmbrellad/src/getumbrellad/controllers/LevelGameplayGUIController.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package appliedlevelgameplaytesting;
+import java.awt.event.*;
+
+
+/**
+ *
+ * @author bruv
+ */
+public class LevelGameplayGUIController implements ActionListener, MouseListener, MouseMotionListener, KeyListener{
+
+    
+    public LevelGameplayGUIController(LevelGameplayPanelGUI panel) {
+        
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {}
+
+    
+    @Override
+    public void mouseClicked(MouseEvent e) {}
+    @Override
+    public void mousePressed(MouseEvent e) {}
+    @Override
+    public void mouseReleased(MouseEvent e) {}
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+    @Override
+    public void mouseExited(MouseEvent e) {}
+    @Override
+    public void mouseDragged(MouseEvent e) {}
+    @Override
+    public void mouseMoved(MouseEvent e) {}
+
+    
+    @Override
+    public void keyTyped(KeyEvent e) {}
+    @Override
+    public void keyPressed(KeyEvent e) {}
+    @Override
+    public void keyReleased(KeyEvent e) {}
+    
+}
+
