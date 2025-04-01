24c04c532a53217abf514ebfb175cd5908d39cf2
SieggyCajucom <sieggycajucom@gmail.com>
2025-04-02 07:52:16 +0800

    Add files via upload

---
+++ GetUmbrellad/src/getumbrellad/views/LevelGameplayGUI.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package appliedlevelgameplaytesting;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author bruv
+ */
+public class LevelGameplayGUI extends JFrame{
+    
+    private LevelGameplayPanelGUI panel;
+    private LevelGameplayGUIController controller; 
+    
+    public LevelGameplayGUI() {
+        super("Level Gameplay");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(900, 600);
+        
+        panel = new LevelGameplayPanelGUI();
+        panel.setLocation(0,0);
+        panel.setSize(this.getSize());
+        panel.setVisible(true);
+        panel.setFocusable(true);
+        this.add(panel);
+        
+        
+        
+        
+        
+        
+        
+        //don't change this
+        addMouseMotionListener(new LevelGameplayPanelGUIController(panel));
+        addKeyListener(new LevelGameplayPanelGUIController(panel));
+        this.setFocusable(true);
+        this.setResizable(false);
+        this.setVisible(true);
+    }
+}
