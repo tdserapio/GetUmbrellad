37b092af4ce18f1e5b8b7f611a476585c55e23b1
SieggyCajucom <sieggycajucom@gmail.com>
2025-04-02 08:02:18 +0800

    Update Player.java

--- GetUmbrellad/src/getumbrellad/models/exceptions/Player.java
+++ GetUmbrellad/src/getumbrellad/models/exceptions/Player.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package appliedlevelgameplaytesting;
+package getumbrellad.models.exception;
 
-/**
- *
- * @author bruv
- */
 public class Player extends Character implements Spawnable{
     private int money;
     
