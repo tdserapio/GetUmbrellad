fe0a9e58d83fd5ef07efa067ae91a6ae669d75dc
SieggyCajucom <sieggycajucom@gmail.com>
2025-04-02 08:02:00 +0800

    Update Obstacle.java

--- GetUmbrellad/src/getumbrellad/models/exceptions/Obstacle.java
+++ GetUmbrellad/src/getumbrellad/models/exceptions/Obstacle.java
@@ -1,17 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package appliedlevelgameplaytesting;
+package getumbrellad.models.exception;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 
-/**
- *
- * @author bruv
- */
 public class Obstacle {
     
     private int x, y, width, height;
