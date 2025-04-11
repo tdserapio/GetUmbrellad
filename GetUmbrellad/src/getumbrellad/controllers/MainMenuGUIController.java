e966f76441218cdcc9979be18087d8ed06666e18
SieggyCajucom <sieggycajucom@gmail.com>
2025-04-11 19:57:07 +0800

    Update MainMenuGUIController.java

--- GetUmbrellad/src/getumbrellad/controllers/MainMenuGUIController.java
+++ GetUmbrellad/src/getumbrellad/controllers/MainMenuGUIController.java
@@ -1,8 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package getumbrellad.controllers;
+
 import getumbrellad.views.AboutGUI;
 import getumbrellad.views.LevelGameplayGUI;
 import getumbrellad.views.HelpGUI;
