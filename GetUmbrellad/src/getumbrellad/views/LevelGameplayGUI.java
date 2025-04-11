4d7c2b7f6965350ec2b6f83001ddfc47be3b5363
SieggyCajucom <sieggycajucom@gmail.com>
2025-04-11 19:58:55 +0800

    Update LevelGameplayGUI.java

--- GetUmbrellad/src/getumbrellad/views/LevelGameplayGUI.java
+++ GetUmbrellad/src/getumbrellad/views/LevelGameplayGUI.java
@@ -1,14 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package appliedlevelgameplaytesting;
-import javax.swing.JFrame;
+package getumbrellad.controllers;
+    
+import getumbrellad.views.LevelGameplayPanelGUI;
+import java.awt.event.*;
 
-/**
- *
- * @author bruv
- */
 public class LevelGameplayGUI extends JFrame{
     
     private LevelGameplayPanelGUI panel;
